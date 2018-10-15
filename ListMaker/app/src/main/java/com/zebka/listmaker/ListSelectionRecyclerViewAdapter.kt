1417d6d474338ae636c6dc075185eb5c511ff207
hugo <hugostar@zoho.com>
2018-10-15 22:36:32 +0300

    new repo

---
+++ ListMaker/app/src/main/java/com/zebka/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.zebka.listmaker
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+    private val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder, parent, false)
+        return ListSelectionViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listTitles.size
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+
+        if (holder != null) {
+            holder.listPosition.text = (position + 1).toString()
+            holder.listTitle.text = listTitles[position]
+        }
+
+    }
+
+
+}
